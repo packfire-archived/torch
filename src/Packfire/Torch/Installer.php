03e0c922ce2f01689ac1176b0cb447dac49230c5
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-19 22:52:30 +0800

    installer implementation

---
+++ src/Packfire/Torch/Installer.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of
+ * Packfire Torch
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\Torch;
+
+/**
+ * Installer that helps to install web assets
+ * 
+ * @author Sam-Mauris Yong <sam@mauris.sg>
+ * @copyright 2012 Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
+ * @package Packfire\Torch
+ * @since 1.0.0
+ * @link https://github.com/packfire/pdc/
+ */
+class Installer {
+    
+    public function __construct(){
+        
+    }
+    
+    /**
+     * Install a web asset
+     * @param array $data The data from the require section
+     * @since 1.0.0
+     */
+    public function install($data){
+        $source = $data['source'];
+        $target = $data['file'];
+        
+        // todo installation process
+    }
+    
+}
