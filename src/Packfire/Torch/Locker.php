f1905679d5472e0594c42fced2e6c7368a4a7928
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-23 09:47:19 +0800

    Locker class implementation

---
+++ src/Packfire/Torch/Locker.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of
+ * Packfire Torch
+ * By Sam-Mauris Yong
+ *
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\Torch;
+
+/**
+ * Lock file generation
+ *
+ * @author Sam-Mauris Yong <sam@mauris.sg>
+ * @copyright 2012 Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
+ * @package Packfire\Torch
+ * @since 1.0.0
+ * @link https://github.com/packfire/torch
+ */
+class Locker {
+    
+    /**
+     * Path to the lock file to generate 
+     * @var string 
+     * @since 1.0.0
+     */
+    private $path;
+    
+    /**
+     * Create a new Locker object
+     * @param string $path Path to the lock file to generate
+     * @since 1.0.0
+     */
+    public function __construct($path){
+        $this->path = $path;
+    }
+    
+    /**
+     * Check whether an entry requiers update
+     * @param array $entry The asset entry
+     * @return boolean Returns true if require update, false otherwise.
+     * @since 1.0.0
+     */
+    public function check($entry){
+        
+    }
+    
+    /**
+     * Perform an asset revision lock
+     * @param array $entry The asset entry
+     * @since 1.0.0
+     */
+    public function lock($entry){
+        
+    }
+    
+}
