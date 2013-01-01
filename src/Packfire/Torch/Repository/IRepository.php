dfd012748bfcd0f6aefa6ea2fe78233a9e8fadfb
Sam-Mauris Yong <mauris@hotmail.sg>
2013-01-02 06:35:51 +0800

    repository interface implementation

---
+++ src/Packfire/Torch/Repository/IRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of
+ * Packfire Torch
+ * By Sam-Mauris Yong
+ *
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\Torch\Repository;
+
+interface IRepository {
+    
+    public function find($name, $version = null);
+    
+}
