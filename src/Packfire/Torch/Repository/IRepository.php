51de2c7184d5b4724ca3e0bcce1d9aff100a3550
Sam-Mauris Yong <mauris@hotmail.sg>
2013-01-02 06:39:56 +0800

    in-code updates for respository interface

--- src/Packfire/Torch/Repository/IRepository.php
+++ src/Packfire/Torch/Repository/IRepository.php
@@ -12,6 +12,16 @@
 
 namespace Packfire\Torch\Repository;
 
+/**
+ * Repository interface
+ * 
+ * @author Sam-Mauris Yong <sam@mauris.sg>
+ * @copyright Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD 3-Clause License
+ * @package Packfire\Torch\Repository
+ * @since 1.1.0
+ * @link https://github.com/packfire/torch
+ */
 interface IRepository {
     
     public function find($name, $version = null);
